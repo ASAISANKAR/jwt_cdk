e799a6dac93bd83b50caf6b6df69ee9820f02759
akulasai <Sai.akula@cdk.com>
2026-06-14 13:55:07 +0530

    added method based security

--- src/main/java/com/springsecurity_jwt/jwt/Config/SecurityConfig.java
+++ src/main/java/com/springsecurity_jwt/jwt/Config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.springsecurity_jwt.jwt.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
@@ -22,7 +23,10 @@ public class SecurityConfig {
     return http
         .authorizeHttpRequests(request -> request
             .requestMatchers("/").permitAll()
-            .requestMatchers("/building","/admin/**").hasAuthority("ROLE_ADMIN")
+            .requestMatchers(HttpMethod.POST, "/building")
+            .hasAuthority("ROLE_ADMIN")
+            .requestMatchers("/admin/**")
+            .hasAuthority("ROLE_ADMIN")
             .anyRequest().authenticated())
         .formLogin(Customizer.withDefaults())
         .httpBasic(Customizer.withDefaults())
